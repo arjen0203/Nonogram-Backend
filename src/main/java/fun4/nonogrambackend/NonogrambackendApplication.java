d38059133dd504177f5bf72862239a66c2c770ff
Arjen <arjenmeer@hotmail.nl>
2020-03-18 11:45:41 +0100

    First init

---
+++ src/main/java/fun4/nonogrambackend/NonogrambackendApplication.java
@@ -0,0 +1,13 @@
+package fun4.nonogrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonogrambackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonogrambackendApplication.class, args);
+    }
+    //yes
+}
---
+++ src/test/java/fun4/nonogrambackend/NonogrambackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fun4.nonogrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonogrambackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
