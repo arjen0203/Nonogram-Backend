e69ff77bff08257408e6c3674e6333f0e27744e5
Arjen <arjenmeer@hotmail.nl>
2020-05-13 14:20:44 +0200

    cleared unnes content

--- src/main/java/fun4/nonogrambackend/controllers/NonogramController.java
+++ src/main/java/fun4/nonogrambackend/controllers/NonogramController.java
@@ -23,10 +23,6 @@ public class NonogramController {
     @Autowired
     private HintTopValueRepository hintTopValueRepository;
 
-    private final long id = 1;
-    private final int[][] topRow = new int[][] { {1}, {5}, {2}, {5}, {2, 1}, {2}};
-    private final int[][] sideRow = new int[][] { {2, 1}, {1, 3}, {1, 2}, {3}, {4}, {1}};
-    
     @CrossOrigin
     @PostMapping(path="/add")
     public ResponseEntity<?> addNewNonogram(@Valid @RequestBody Nonogram nonogram) {
