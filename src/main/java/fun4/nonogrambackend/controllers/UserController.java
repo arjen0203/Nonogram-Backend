1004d105324ac77ba56c53e84404e3478c832643
Arjen <arjenmeer@hotmail.nl>
2020-05-29 20:42:44 +0200

    getting user data based on usertoken

--- src/main/java/fun4/nonogrambackend/controllers/UserController.java
+++ src/main/java/fun4/nonogrambackend/controllers/UserController.java
@@ -1,13 +1,18 @@
 package fun4.nonogrambackend.controllers;
 
+import com.auth0.jwt.JWT;
 import fun4.nonogrambackend.domain.Nonogram;
 import fun4.nonogrambackend.repositories.UserRepository;
 import fun4.nonogrambackend.domain.User;
 import fun4.nonogrambackend.security.JWTAuthenticationFilter;
+import fun4.nonogrambackend.security.JWTAuthorizationFilter;
 import fun4.nonogrambackend.services.UserDetailsServiceImpl;
 import fun4.nonogrambackend.services.UserService;
 import fun4.nonogrambackend.validators.UserValidator;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,11 +30,28 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
     private final UserValidator userValidator;
+    private final UserRepository userRepository;
 
 
-    public UserController(UserService userService, UserValidator userValidator) {
+    public UserController(UserService userService, UserValidator userValidator, UserRepository userRepository) {
         this.userService = userService;
         this.userValidator = userValidator;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getMe(@RequestHeader("Authorization") String token) {
+        token = token.replace("Bearer ", "");
+        String decodedToken = new String(Base64.decodeBase64(JWT.decode(token).getPayload()));
+        try {
+            JSONObject json = new JSONObject(decodedToken);
+            String username = json.get("sub").toString();
+            Optional<User> user = userRepository.findByUsername(username);
+            if (user.isEmpty()) return ResponseEntity.status(404).body("User not found");
+            return ResponseEntity.ok(user.get());
+        } catch (JSONException e) {
+            return ResponseEntity.status(403).body("Unauthorized request");
+        }
     }
 
     @CrossOrigin
--- src/main/java/fun4/nonogrambackend/domain/User.java
+++ src/main/java/fun4/nonogrambackend/domain/User.java
@@ -14,6 +14,7 @@ public class User {
     @Column(unique = true)
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public long getId() {
